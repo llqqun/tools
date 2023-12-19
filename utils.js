19061030289261f4a6b0f310fe172b2ef5a8d511
lance L <36347513+llqqun@users.noreply.github.com>
2023-12-19 10:17:43 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -21,3 +21,27 @@ export default function throttle(fn, interval, callFirst) {
     }
   };
 }
+
+// 防抖
+export default function debounce(fn, wait, callFirst) {
+  let timeout;
+  return function() {
+    if (!wait) {
+      return fn.apply(this, arguments);
+    }
+    let context = this;
+    let args = arguments;
+    let callNow = callFirst && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      timeout = null;
+      if (!callNow) {
+        return fn.apply(context, args);
+      }
+    }, wait);
+
+    if (callNow) {
+      return fn.apply(this, arguments);
+    }
+  };
+}
